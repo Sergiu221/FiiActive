c1f928bde4c8223d3cee53d17c5d9978422b1933
Sergiu221 <sergiu221@gmail.com>
2025-02-14 19:13:10 +0200

    Apply view compact

--- app/src/main/java/com/org/fiiactiv/OptionsActivity.java
+++ app/src/main/java/com/org/fiiactiv/OptionsActivity.java
@@ -4,15 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class OptionsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.options_activity);
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
         // Find the "Go Back" button in the layout
         Button goBackButton = findViewById(R.id.btn_go_back);
 
